bc6952a3f53c128d625cdedee518593f36f69f5f
Vivek Kosare <v_kosare@yahoo.com>
2024-01-07 21:23:35 +0100

    expense item data iteration using map()

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -32,26 +32,15 @@ const Expenses = (props) => {
       <Card className="expenses">
       <ExpenseFilter selected={filteredYear} onYearSelection = {yearSelectionHandler}/>
       <p className="expenses-filter">Data for year {filteredInfoText} is hidden</p>
-        <ExpensesItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        ></ExpensesItem>
-        <ExpensesItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        ></ExpensesItem>
-        <ExpensesItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        ></ExpensesItem>
-        <ExpensesItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        ></ExpensesItem>
+        {props.expenses.map((expense) => {
+          return (
+            <ExpensesItem
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date} 
+            ></ExpensesItem>        
+          );     
+        })}
       </Card>
     </div>
   );
